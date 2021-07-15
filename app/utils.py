27717eb1ae4aa1c4afad8e1ef01d2e5eece6cdaa
Luke Miller <millerluke1364@gmail.com>
2021-07-15 18:35:08 -0400

    added persistent json object

---
+++ app/utils.py
@@ -0,0 +1,29 @@
+import json
+from pathlib import Path
+
+class PersistentJSON:
+    def __init__(self, filename):
+        self.filename = filename
+        self.db = {}
+
+        if Path(self.filename).exists():
+            self._load()
+        else:
+            self._save()
+
+    def _load(self):
+        file = open(self.filename, 'r')
+        self.db = json.load(file)
+        file.close()
+    
+    def _save(self):
+        file = open(self.filename, 'w')
+        json.dump(self.db, file)
+        file.close()
+
+    def __getitem__(self, key):
+        return self.db[key]
+
+    def __setitem__(self, key, val):
+        self.db[key] = val
+        self._save()
