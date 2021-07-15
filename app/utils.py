af53bf88d722c8b9858774e1f84f215106400bee
Luke Miller <millerluke1364@gmail.com>
2021-07-15 19:24:50 -0400

    added str and repr functions

--- app/utils.py
+++ app/utils.py
@@ -7,11 +7,12 @@ class PersistentJSON:
         self.filename = filename
         self.db = {}
 
+        # Creates empty json if file doesn't already exist
         if Path(self.filename).exists():
             self._load()
         else:
             self._save()
-
+    
     def _load(self):
         file = open(self.filename, 'r')
         self.db = json.load(file)
@@ -19,15 +20,23 @@ class PersistentJSON:
     
     def _save(self):
         file = open(self.filename, 'w')
-        json.dump(self.db, file)
+        json.dump(self.db, file, indent=4)
         file.close()
 
+    # Internal Python functions allow the object to be interacted with like a dictionary using []
     def __getitem__(self, key):
         return self.db[key]
 
     def __setitem__(self, key, val):
         self.db[key] = val
         self._save()
+    
+    # Object represented as a string of the dictionary
+    def __repr__(self):
+        return str(self.db)
+
+    def __str__(self):
+        return str(self.db)
 
 def user_to_color(user: discord.User):
     '''Maps discord discriminator to a hex color value.'''
