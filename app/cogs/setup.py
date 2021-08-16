aff68f9bc1353b881c10711756e288416ba7feea
Luke Miller <millerluke1364@gmail.com>
2021-08-16 18:32:32 -0400

    added setup command for easy setup in satellite servers.

---
+++ app/cogs/setup.py
@@ -0,0 +1,39 @@
+from discord import guild
+from discord.ext import commands
+from discord.ext.commands import cog
+from discord_slash import cog_ext
+from constants import CENTRAL_HUB_ID
+from database import *
+
+from cogs.curator import CuratorCog
+
+class SetupCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @cog_ext.cog_slash(
+        name="setup",
+        guild_ids=[474736509472473088, 870551183339696138, 870551292525809684]
+    )
+    async def setup(self, ctx):
+        observatory = self.bot.get_guild(CENTRAL_HUB_ID)
+        
+        if ctx.guild == observatory:
+            await ctx.send("Setup can only be run in Satellite Servers.")
+            return
+
+        # creating category and channels in Observatory
+        category = await observatory.create_category(ctx.guild.name)
+        bridge   = await observatory.create_text_channel("Bridge", category=category)
+        pending  = await observatory.create_text_channel("Pending Messages", category=category)
+        approved = await observatory.create_text_channel("Approved Messages", category=category)
+
+        # setting channel ids for curation process
+        db.guild(ctx.guild).pending_channel = pending
+        db.guild(ctx.guild).approved_channel = approved
+        
+        await ctx.send("Done!")
+
+def setup(bot):
+    cog = SetupCog(bot)
+    bot.add_cog(cog)
--- app/constants.py
+++ app/constants.py
@@ -1,7 +1,9 @@
 TOKEN_ENV_NAME = 'DISCORD_API_TOKEN'
 DATABASE_FNAME = 'data.json'
 COMMAND_PREFIX = '.'
+CENTRAL_HUB_ID = 474736509472473088
 EXTENSIONS = [
     'cogs.curator',
-    'cogs.admin'
+    'cogs.admin',
+    'cogs.setup'
 ]
